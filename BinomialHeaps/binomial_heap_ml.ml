b69bacbfd7de6b1ac2227db05947733b1c2abbf3
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-11-18 16:23:53 +0900

    test code is red...

--- BinomialHeaps/binomial_heap_ml.ml
+++ BinomialHeaps/binomial_heap_ml.ml
@@ -1,3 +1,5 @@
+open List
+
 type comparison = Less | Equal | Greater
 
 module type OrderedType =
@@ -16,9 +18,38 @@ module BinomialHeap =
       let empty = []
       let isEmpty ts = ts = []
       let rank = function
-          Node(r, _, _) -> r
+        | Node(r, _, _) -> r
       let root = function
-          Node(_, x, _) -> x
+	| Node(_, x, _) -> x
+      let link (Node(r, x1, c1) as t1) (Node(_, x2, c2) as t2) =
+	if Ord.compare x1 x2 = Less then Node(r + 1, x1, t2 :: c1)
+	else Node(r + 1, x2, t1 :: c2)
+      let rec insTree t = function
+        | [] -> [t]
+	| (t' :: ts' as ts) ->
+	  if rank t < rank t' then t :: ts else insTree (link t t') ts'
+      let insert x ts = insTree (Node(0, x, [])) ts
+      let rec insertAll a1 a2 = match (a1, a2) with
+	| ([], ts) -> ts
+	| (x :: xs, ts) -> insertAll xs (insert x ts)
+      let rec merge a1 a2 = match (a1, a2) with
+	| (ts, []) -> ts
+	| ([], ts) -> ts
+	| ((t1 :: ts1' as ts1), (t2 :: ts2' as ts2)) ->
+	  if rank t1 < rank t2 then t1 :: merge ts1' ts2
+	  else if rank t2 < rank t1 then t2 :: merge ts1 ts2'
+	  else insTree (link t1 t2) (merge ts1' ts2')
+      let rec removeMinTree = function
+	| [] -> raise (Failure "removeMinTree: don't call with null list.")
+	| [t] -> (t, [])
+	| (t :: ts) ->
+	  let (t', ts') = removeMinTree ts
+	  in if Ord.compare (root t') (root t) = Greater
+	    then (t', t :: ts') else (t, ts)
+      let findMin ts = let (t, _) = removeMinTree ts in root t
+      let deleteMin ts =
+	let (Node(_, x, ts1), ts2) = removeMinTree ts
+	in merge (rev ts1) ts2
     end
 
 module OrderedInt =
@@ -28,5 +59,3 @@ module OrderedInt =
   end
  
 module BinomialHeapInt = BinomialHeap(OrderedInt)
-
-let hoge = 1
--- BinomialHeaps/binomial_heap_test_ml.ml
+++ BinomialHeaps/binomial_heap_test_ml.ml
@@ -1,8 +1,10 @@
 open OUnit
 open Binomial_heap_ml
 
-let test_1 _ =
-  assert_equal BinomialHeapInt.empty []
+let test_1 _ = let found = BinomialHeapInt.findMin
+(*		 (BinomialHeapInt.insertAll [2;3;1;4;5;9;8;7;6] []) *)
+		 (BinomialHeapInt.insertAll [1;2;3] [])
+	       in assert_equal 1 found
 
 let suite = "Test BinomialHeap" >:::
   ["test_1" >:: test_1]
