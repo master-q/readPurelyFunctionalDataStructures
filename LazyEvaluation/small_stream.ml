19b051a1ac0eccd901132016c6e61b412437202c
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-12-22 21:27:44 +0900

    add repeat function and it's test code

--- LazyEvaluation/small_stream.ml
+++ LazyEvaluation/small_stream.ml
@@ -1,27 +1,114 @@
+(* http://hackage.haskell.org/packages/archive/base/latest/doc/html/Prelude.html *)
+
 type 'a cell = SSnil | SScons of 'a * 'a stream
 and 'a stream = 'a cell Lazy.t
 
-(* let hoge = lazy (SScons(2, lazy (SScons(1, lazy SSnil)))) *)
+(* *** List operations *)
+
+(* map *)
 
 let (++) t1 t2 =
   let rec pp t1 t2 = match t1 with
     | (lazy SSnil) -> t2
     | (lazy (SScons(x, s))) -> lazy (SScons(x, pp s t2))
   in lazy (pp t1 t2)
+
+(* filter *)
+(* head *)
+(* last *)
+(* tail *)
+(* init *)
+(* null *)
+(* length *)
+(* !! *)
+
+let reverse s =
+  let rec reverse' = function
+    | (lazy SSnil, r) -> r
+    | (lazy (SScons(x, s)), r) -> reverse' (s, lazy (SScons(x, r)))
+  in lazy (reverse' (s, lazy SSnil))
+
+(* *** Reducing lists (folds) *)
+
+let foldr f z s =
+  let rec foldr' f z s = match (f, z, s) with
+    | (_, z, lazy SSnil) -> z
+    | (f, z, lazy (SScons(x, s))) -> f x (foldr' f z s)
+  in lazy (foldr' f z s)
+
+(* foldr1 *)
+
+(* *** Special folds *)
+
+(* and *)
+(* or *)
+(* any *)
+(* all *)
+(* sum *)
+(* product *)
+(* concat *)
+(* concatMap *)
+(* maximum *)
+(* minimum *)
+
+(* *** Building lists *)
+(* *** scan *)
+
+(* scanl *)
+(* scanl1 *)
+(* scanr *)
+(* scanr1 *)
+
+(* *** Infinite lists *)
+
+(* iterate *)
+let repeat x = let rec xs = lazy (SScons(x, xs))
+	       in xs
+
+(* replicate *)
+(* cycle *)
+
+(* *** Sublists *)
+
 let take n s =
   let rec take' n' s' = match (n', s') with
     | (0, _) -> lazy SSnil
     | (_, lazy SSnil) -> lazy SSnil
     | (n'', lazy (SScons(x, s''))) -> lazy (SScons(x, take' (n'' - 1) s''))
   in lazy (take' n s)
+
+
 let drop n s =
   let rec drop' n' s' = match (n', s') with
     | (0, s'') -> s''
     | (_, lazy SSnil) -> lazy SSnil
     | (n'', lazy (SScons(_, s'''))) -> drop' (n'' - 1) s'''
-  in lazy (drop' n s)
-let reverse s =
-  let rec reverse' = function
-    | (lazy SSnil, r) -> r
-    | (lazy (SScons(x, s)), r) -> reverse' (s, lazy (SScons(x, r)))
-  in lazy (reverse' (s, lazy SSnil))
+  in drop' n s
+
+(* splitAt *)
+(* takeWhile *)
+(* dropWhile *)
+(* span *)
+(* break *)
+
+(* *** Searching lists *)
+
+(* elem *)
+(* notElem *)
+(* lookup *)
+
+(* *** Zipping and unzipping lists *)
+
+(* zip *)
+(* zip3 *)
+(* zipWith *)
+(* zipWith3 *)
+(* unzip *)
+(* unzip3 *)
+
+(* *** Functions on strings *)
+
+(* lines *)
+(* words *)
+(* unlines *)
+(* unwords *)
--- LazyEvaluation/small_stream_test.ml
+++ LazyEvaluation/small_stream_test.ml
@@ -1,12 +1,22 @@
 open OUnit
 open Small_stream
 
+let stream_321 =
+  lazy (SScons(3, (lazy (SScons(2, lazy (SScons(1, lazy SSnil)))))))
+
+let stream_inf = repeat 1
+
+let test_0 _ = match drop 100000 stream_inf with
+  | (lazy SSnil) -> assert_failure "stream_inf has no SSnil."
+  | (lazy (SScons(x, _))) -> assert_equal 1 x
+
 let test_1 _ = let st = drop 1 (lazy (SScons(2, lazy (SScons(1, lazy SSnil)))))
-	       in match Lazy.force st with
-		 | (lazy SSnil) -> assert_equal 0 1 (* error *)
+	       in match st with
+		 | (lazy SSnil) -> assert_failure "stream has only one elm."
 		 | (lazy (SScons(x, _))) -> assert_equal 1 x
 
 let suite = "Test SmallStream" >:::
-  ["test_1" >:: test_1]
+  ["test_0" >:: test_0;
+   "test_1" >:: test_1]
 
 let _ = run_test_tt_main suite
