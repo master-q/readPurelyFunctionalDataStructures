aa0bddd9b5d7fca7d434cad355074e8066ecfe28
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-01-16 23:41:30 +0900

    test like benchmark

--- RedBlackTree/redblack_tree_test.ml
+++ RedBlackTree/redblack_tree_test.ml
@@ -1,15 +1,22 @@
 open OUnit
 open Redblack_tree
 
+let rec insert_list t l = match l with
+  | x::xs -> insert_list (insert x t) xs
+  | [] -> t
+
+let rec insert_times t n =
+  if n > 0 then insert_times (insert (Random.int 10000000) t) (n - 1)
+  else t
+
 let test1 _ =
-  let rec f t l = match l with
-    | x::xs -> f (insert x t) xs
-    | [] -> t in
-  let rbt = f E [3;2;4;5;6;9;0] in 
-  assert (member 0 rbt)
+  let rbt = insert_times E 1000000 in
+  let rbt0 = insert 0 rbt in
+  assert (member 0 rbt0)
 
-let suite = "Test SmallStream" >:::
-  ["test1"  >:: test1;
-  ]
+let suite = "Test RedBlackTree" >:::
+  ["test1" >:: test1]
 
-let _ = run_test_tt_main suite
+let _ =
+  Random.init 1;
+  run_test_tt_main suite
