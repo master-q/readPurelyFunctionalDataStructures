aa24940750da52b0ea5be01673193934a2c439c3
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-01-17 10:59:07 +0900

    test insert sorted numbers

--- RedBlackTree/redblack_tree_testbase.ml
+++ RedBlackTree/redblack_tree_testbase.ml
@@ -4,14 +4,23 @@ let rec insert_list fi t l = match l with
   | x::xs -> insert_list fi (fi x t) xs
   | [] -> t
 
-let rec insert_times fi t n =
-  if n > 0 then insert_times fi (fi (Random.int 10000000) t) (n - 1)
+let rec insert_times_rand fi t n =
+  if n > 0 then insert_times_rand fi (fi (Random.int 10000000) t) (n - 1)
   else t
 
-let test1 fi fm te _ =
-  let rbt = insert_times fi te 1000000 in
+let rec insert_times_incl fi t until n =
+  if n < until then insert_times_incl fi (fi n t) until (n + 1)
+  else t
+
+let test_rand fi fm te _ =
+  let rbt = insert_times_rand fi te 1000000 in
   let rbt0 = fi 0 rbt in
   assert (fm 0 rbt0)
 
+let test_sorted fi fm te _ =
+  let rbt = insert_times_incl fi te 1000000 0 in
+  assert (fm 0 rbt)
+
 let suite fi fm te = "Test RedBlackTree" >:::
-  ["test1" >:: (test1 fi fm te)]
+  ["test_rand" >:: (test_rand fi fm te);
+   "test_sorted" >:: (test_sorted fi fm te);]
