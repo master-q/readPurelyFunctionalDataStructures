17f262147844a1baa606c422b8c4f932b0f07511
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-01-20 20:30:50 +0900

    ttoooooooooo late ex3_10b

--- RedBlackTree/redblack_tree_ex3_10b.ml
+++ RedBlackTree/redblack_tree_ex3_10b.ml
@@ -1,5 +1,6 @@
 type color = R | B
 type 'a tree = E | T of color * 'a tree * 'a * 'a tree
+type way = Left | Right | DontKnow
 
 let rec member x t = match t with
   | E             -> false
@@ -7,22 +8,27 @@ let rec member x t = match t with
     if x < y then member x a
     else if x > y then member x b else true
 
-let lbalance c t1 x t2 = match (c, t1, x, t2) with
-  | (B, T(R, T(R,a,x,b), y, c), z, d) -> T(R, T(B,a,x,b), y, T(B,c,z,d))
-  | (B, T(R, a, x, T(R,b,y,c)), z, d) -> T(R, T(B,a,x,b), y, T(B,c,z,d))
-  | (c, t1, x, t2) -> T(c, t1, x, t2)
+let lbalance c wt1 x t2 = match (c, wt1, x, t2) with
+  | (B, (Left, T(R, T(R,a,x,b), y, c)), z, d) ->
+    T(R, T(B,a,x,b), y, T(B,c,z,d))
+  | (B, (Right, T(R, a, x, T(R,b,y,c))), z, d) ->
+    T(R, T(B,a,x,b), y, T(B,c,z,d))
+  | (c, (_, t1), x, t2) -> T(c, t1, x, t2)
 
-let rbalance c t1 x t2 = match (c, t1, x, t2) with
-  | (B, a, x, T(R, T(R,b,y,c), z, d)) -> T(R, T(B,a,x,b), y, T(B,c,z,d))
-  | (B, a, x, T(R, b, y, T(R,c,z,d))) -> T(R, T(B,a,x,b), y, T(B,c,z,d))
-  | (c, t1, x, t2) -> T(c, t1, x, t2)
+let rbalance c t1 x wt2 = match (c, t1, x, wt2) with
+  | (B, a, x, (Left, T(R, T(R,b,y,c), z, d))) ->
+    T(R, T(B,a,x,b), y, T(B,c,z,d))
+  | (B, a, x, (Right, T(R, b, y, T(R,c,z,d)))) ->
+    T(R, T(B,a,x,b), y, T(B,c,z,d))
+  | (c, t1, x, (_, t2)) -> T(c, t1, x, t2)
 
 let insert x s =
   let rec ins q = match q with
-    | E -> T(R, E, x, E)
+    | E -> (DontKnow, T(R, E, x, E))
     | T(color, a, y, b) as s ->
-      if x < y then lbalance color (ins a) y b
-      else if x > y then rbalance color a y (ins b) else s in
+      if x < y then (Left, lbalance color (ins a) y b) else
+	if x > y then (Right, rbalance color a y (ins b))
+	else (DontKnow, s) in
   match ins s with
-    | T(_, a, y, b) -> T(B, a, y, b)
-    | E -> assert false
+    | (_, T(_, a, y, b)) -> T(B, a, y, b)
+    | (_, E) -> assert false
