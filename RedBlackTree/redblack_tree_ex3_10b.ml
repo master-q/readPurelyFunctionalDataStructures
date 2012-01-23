b494630f19385f152ceee0ce5dfc112a095691c6
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-01-23 16:58:23 +0900

    1to100pen's answer for ex3.10b

--- RedBlackTree/redblack_tree_ex3_10b.ml
+++ RedBlackTree/redblack_tree_ex3_10b.ml
@@ -1,6 +1,5 @@
 type color = R | B
 type 'a tree = E | T of color * 'a tree * 'a * 'a tree
-type way = Left | Right | DontKnow
 
 let rec member x t = match t with
   | E             -> false
@@ -8,27 +7,40 @@ let rec member x t = match t with
     if x < y then member x a
     else if x > y then member x b else true
 
-let lbalance c wt1 x t2 = match (c, wt1, x, t2) with
-  | (B, (Left, T(R, T(R,a,x,b), y, c)), z, d) ->
-    T(R, T(B,a,x,b), y, T(B,c,z,d))
-  | (B, (Right, T(R, a, x, T(R,b,y,c))), z, d) ->
-    T(R, T(B,a,x,b), y, T(B,c,z,d))
-  | (c, (_, t1), x, t2) -> T(c, t1, x, t2)
+let llbalance c t1 x t2 = match (c, t1, x, t2) with
+  | (B, T(R, T(R,a,x,b), y, c), z, d) -> T(R, T(B,a,x,b), y, T(B,c,z,d))
+  | (c, t1, x, t2) -> T(c, t1, x, t2)
 
-let rbalance c t1 x wt2 = match (c, t1, x, wt2) with
-  | (B, a, x, (Left, T(R, T(R,b,y,c), z, d))) ->
-    T(R, T(B,a,x,b), y, T(B,c,z,d))
-  | (B, a, x, (Right, T(R, b, y, T(R,c,z,d)))) ->
-    T(R, T(B,a,x,b), y, T(B,c,z,d))
-  | (c, t1, x, (_, t2)) -> T(c, t1, x, t2)
+let lrbalance c t1 x t2 = match (c, t1, x, t2) with
+  | (B, T(R, a, x, T(R,b,y,c)), z, d) -> T(R, T(B,a,x,b), y, T(B,c,z,d))
+  | (c, t1, x, t2) -> T(c, t1, x, t2)
+
+let rlbalance c t1 x t2 = match (c, t1, x, t2) with
+  | (B, a, x, T(R, T(R,b,y,c), z, d)) -> T(R, T(B,a,x,b), y, T(B,c,z,d))
+  | (c, t1, x, t2) -> T(c, t1, x, t2)
+
+let rrbalance c t1 x t2 = match (c, t1, x, t2) with
+  | (B, a, x, T(R, b, y, T(R,c,z,d))) -> T(R, T(B,a,x,b), y, T(B,c,z,d))
+  | (c, t1, x, t2) -> T(c, t1, x, t2)
 
 let insert x s =
-  let rec ins q = match q with
-    | E -> (DontKnow, T(R, E, x, E))
-    | T(color, a, y, b) as s ->
-      if x < y then (Left, lbalance color (ins a) y b) else
-	if x > y then (Right, rbalance color a y (ins b))
-	else (DontKnow, s) in
+  let rec insl q = match q with
+    | T(color, E, v, r) -> T(color, T(R, E, x, E), v, r)
+    | T(color, (T(_, _, v2, _) as l), v, r) as n ->
+      if x < v2 then llbalance color (insl l) v r
+      else if x > v2 then lrbalance color (insr l) v r else n
+    | E -> T(R, E, x, E)
+  and insr q = match q with
+    | T(color, l, v, E) -> T(color, l, v, T(R, E, x, E))
+    | T(color, l, v, (T(_, _, v2, _) as r)) as n ->
+      if x < v2 then rlbalance color l v (insl r)
+      else if x > v2 then rrbalance color l v (insr r) else n
+    | E -> T(R, E, x, E) in
+  let ins q = match q with
+    | E -> T(R, E, x, E)
+    | T(_, _, v, _) as n ->
+      if x < v then insl n
+      else if x > v then insr n else n in
   match ins s with
-    | (_, T(_, a, y, b)) -> T(B, a, y, b)
-    | (_, E) -> assert false
+    | T(_, a, y, b) -> T(B, a, y, b)
+    | E -> assert false
