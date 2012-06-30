11e16f159ce56adeae91e43f031efe97a7797753
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-06-30 16:34:00 +0900

    bug fix for susp show

--- RealTimeQueue/realtime_queue_test.ml
+++ RealTimeQueue/realtime_queue_test.ml
@@ -3,26 +3,26 @@ open Printf
 open Realtime_queue
 
 let test_123 _ =
-  let q1     = RTQueue.snoc RTQueue.empty 1 in
-  let q12    = RTQueue.snoc q1 2 in
-  let q123   = RTQueue.snoc q12 3 in
-  let q1234  = RTQueue.snoc q123 4 in
-  let q12345 = RTQueue.snoc q1234 5 in
-  let q2345  = RTQueue.tail q12345 in
-  let q345   = RTQueue.tail q2345 in
-  let q45    = RTQueue.tail q345 in
-  let q5     = RTQueue.tail q45 in
-  let qe     = RTQueue.tail q5 in
   printf "%s\n" (RTQueue.show RTQueue.empty);
+  let q1     = RTQueue.snoc RTQueue.empty 1 in
   printf "%s\n" (RTQueue.show q1);
+  let q12    = RTQueue.snoc q1 2 in
   printf "%s\n" (RTQueue.show q12);
+  let q123   = RTQueue.snoc q12 3 in
   printf "%s\n" (RTQueue.show q123);
+  let q1234  = RTQueue.snoc q123 4 in
   printf "%s\n" (RTQueue.show q1234);
+  let q12345 = RTQueue.snoc q1234 5 in
   printf "%s\n" (RTQueue.show q12345);
+  let q2345  = RTQueue.tail q12345 in
   printf "%s\n" (RTQueue.show q2345);
+  let q345   = RTQueue.tail q2345 in
   printf "%s\n" (RTQueue.show q345);
+  let q45    = RTQueue.tail q345 in
   printf "%s\n" (RTQueue.show q45);
+  let q5     = RTQueue.tail q45 in
   printf "%s\n" (RTQueue.show q5);
+  let qe     = RTQueue.tail q5 in
   printf "%s\n" (RTQueue.show qe);
   assert_equal 1 (RTQueue.head q12345);
   assert_equal 2 (RTQueue.head q2345);
