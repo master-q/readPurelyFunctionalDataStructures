dc898d948398cbe40696dae8676035076bf4e3f5
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-11-02 22:17:19 +0900

    remove comment

--- PolymorphicRecursion/Haskell/MySeq.hs
+++ PolymorphicRecursion/Haskell/MySeq.hs
@@ -5,11 +5,3 @@ data MySeq a = MSNil | MSCons (a, MySeq (a,a))
 sizeS :: MySeq a -> Int
 sizeS MSNil = 0
 sizeS (MSCons (_, ps)) = 1 + 2 * sizeS ps
-
-{--
-main :: IO ()
-main = do
-  print $ sizeS MSNil
-  print $ sizeS (MSCons (1, MSNil))
-  print $ sizeS (MSCons (1, MSCons ((2, 3), MSNil)))
---}
