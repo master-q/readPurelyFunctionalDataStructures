e849c2f9db0b63781c4b817a3605311ee5f02976
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-11-02 22:16:48 +0900

    add test code for PolymorphicRecursion on Haskell

--- PolymorphicRecursion/Haskell/MySeq.hs
+++ PolymorphicRecursion/Haskell/MySeq.hs
@@ -1,11 +1,15 @@
+module MySeq (MySeq(..), sizeS) where
+
 data MySeq a = MSNil | MSCons (a, MySeq (a,a))
 
 sizeS :: MySeq a -> Int
 sizeS MSNil = 0
 sizeS (MSCons (_, ps)) = 1 + 2 * sizeS ps
 
+{--
 main :: IO ()
 main = do
   print $ sizeS MSNil
   print $ sizeS (MSCons (1, MSNil))
   print $ sizeS (MSCons (1, MSCons ((2, 3), MSNil)))
+--}
---
+++ PolymorphicRecursion/Haskell/MySeqTest.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+import Test.Framework.TH
+import Test.Framework
+import Test.HUnit
+import Test.Framework.Providers.HUnit
+import MySeq
+
+fooTestGroup :: Test.Framework.Test
+fooTestGroup = $(testGroupGenerator)
+
+main :: IO ()
+main = defaultMain [fooTestGroup]
+
+case_0 :: Assertion
+case_0 = do 0 @=? sizeS MSNil
+
+case_1 :: Assertion
+case_1 = do 1 @=? sizeS (MSCons (1, MSNil))
+
+case_3 :: Assertion
+case_3 = do 3 @=? sizeS (MSCons (1, MSCons ((2, 3), MSNil)))
