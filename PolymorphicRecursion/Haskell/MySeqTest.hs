32d051f941003d9a136216961b7c4deb1e776b7c
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-11-02 22:20:42 +0900

    remove do to fix hlint warn

--- PolymorphicRecursion/Haskell/MySeqTest.hs
+++ PolymorphicRecursion/Haskell/MySeqTest.hs
@@ -12,10 +12,10 @@ main :: IO ()
 main = defaultMain [fooTestGroup]
 
 case_0 :: Assertion
-case_0 = do 0 @=? sizeS MSNil
+case_0 = 0 @=? sizeS MSNil
 
 case_1 :: Assertion
-case_1 = do 1 @=? sizeS (MSCons (1, MSNil))
+case_1 = 1 @=? sizeS (MSCons (1, MSNil))
 
 case_3 :: Assertion
-case_3 = do 3 @=? sizeS (MSCons (1, MSCons ((2, 3), MSNil)))
+case_3 = 3 @=? sizeS (MSCons (1, MSCons ((2, 3), MSNil)))
